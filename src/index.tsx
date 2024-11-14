30e3f7cb51955569640a0bfc6b5e1e4c456f641a
OscarJimenezz <114299099+OscarJimenezz@users.noreply.github.com>
2024-11-14 03:05:56 +0300

    optimization update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const renderApp = () => {
-  const rootElement = document.getElementById('root');
+  const rootElement = document.getElementById('root') || document.createElement('div');
+  
+  if (!document.getElementById('root')) {
+    rootElement.id = 'root';
+    document.body.appendChild(rootElement);
+  }
+
   ReactDOM.render(<App />, rootElement);
 };
 
